8fd05321cde7d44f48e7ae225a80bc8e52b0cd4a
Chaitra74 <chai7495@gmail.com>
2022-05-09 14:33:45 +0530

    Add files via upload

--- app/statistics.js
+++ app/statistics.js
@@ -1,5 +1,14 @@
 module.exports=function getStatistics(numbers){
-    
     //implement the computation of statistics here
+    var total = 0;  
+    let statsObj = {};
+    statsObj.min = Math.min(...numbers);  
+    statsObj.max = Math.max(...numbers);
+    for(var i = 0; i < numbers.length; i++)
+    {
+        total += numbers[i];
+    }
+    statsObj.average = total / numbers.length;   
+    return statsObj;   
 }
 
